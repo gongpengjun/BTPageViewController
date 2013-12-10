6ef5eb606edf3eda98db720b6bc5b2c34824a035
gongpengjun <frank.gongpengjun@gmail.com>
2013-12-10 17:04:58 +0800

    initial commit

---
+++ BTPageViewControllerDemo/BTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTAppDelegate.h
+//  BTPageViewControllerDemo
+//
+//  Created by 巩 鹏军 on 13-12-10.
+//  Copyright (c) 2013年 巩 鹏军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BTPageViewControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTPageViewControllerDemo
+//
+//  Created by 巩 鹏军 on 13-12-10.
+//  Copyright (c) 2013年 巩 鹏军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BTAppDelegate class]));
+    }
+}
